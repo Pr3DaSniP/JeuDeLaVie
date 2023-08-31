092f7a2c4dfa8062c61826414224474c8127b6cd
GEILLER Valentin <geiller.valentin@free.fr>
2023-08-31 17:48:43 +0200

    add project file

---
+++ JeuDeLaVie/Source.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <windows.h>
+#include <time.h>
+
+#include "olcConsoleGameEngine.h"
+
+using namespace std;
+
+
+class JeuDeLaVie : public olcConsoleGameEngine
+{
+private:
+	const int NB_CASES = 60;
+	vector<vector<int>> grille;
+
+	void init(vector<vector<int>>& tab) {
+		for (int i = 0; i < tab.size(); i++) {
+			for (int j = 0; j < tab[i].size(); j++) {
+				tab[i][j] = rand() % 2;
+			}
+		}
+	}
+
+	vector<vector<int>> calculeVoisins(const vector<vector<int>>& grille) {
+		vector<vector<int>> voisins(grille.size(), vector<int>(grille[0].size()));
+		for (int i = 0; i < voisins.size(); i++) {
+			for (int j = 0; j < voisins[i].size(); j++) {
+				voisins[i][j] = 0;
+				if (i > 0) {
+					voisins[i][j] += grille[i - 1][j];
+				}
+				if (i < grille.size() - 1) {
+					voisins[i][j] += grille[i + 1][j];
+				}
+				if (j > 0) {
+					voisins[i][j] += grille[i][j - 1];
+				}
+				if (j < 9) {
+					voisins[i][j] += grille[i][j + 1];
+				}
+				if (i > 0 && j > 0) {
+					voisins[i][j] += grille[i - 1][j - 1];
+				}
+				if (i > 0 && j < grille.size() - 1) {
+					voisins[i][j] += grille[i - 1][j + 1];
+				}
+				if (i < grille.size() - 1 && j > 0) {
+					voisins[i][j] += grille[i + 1][j - 1];
+				}
+				if (i < grille.size() - 1 && j < grille.size() - 1) {
+					voisins[i][j] += grille[i + 1][j + 1];
+				}
+			}
+		}
+		return voisins;
+	}
+
+public:
+	int i = 0;
+
+	JeuDeLaVie()
+	{
+		m_sAppName = L"Game Life";
+	}
+
+	void affiche() 
+	{
+		for (int i = 0; i < grille.size(); i++)
+		{
+			for (int j = 0; j < grille[i].size(); j++)
+			{
+				if (grille[i][j] == 1)
+				{
+					Draw(i, j ,PIXEL_SOLID, FG_WHITE);
+				}
+				else
+				{
+					Draw(i, j, PIXEL_SOLID, FG_BLACK);
+				}
+			}
+		}
+	}
+
+protected:
+	virtual bool OnUserCreate()
+	{
+		grille.resize(NB_CASES, vector<int>(NB_CASES));
+		init(grille);
+
+		return true;
+	}
+
+	virtual bool OnUserUpdate(float fElapsedTime)
+	{
+		this_thread::sleep_for(50ms);
+		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+
+		vector<vector<int>> voisins = calculeVoisins(grille);
+
+		for (int i = 0; i < grille.size(); i++) {
+			for (int j = 0; j < grille[i].size(); j++) {
+				if (grille[i][j] == 1) {
+					if (voisins[i][j] < 2 || voisins[i][j] > 3) {
+						grille[i][j] = 0;
+					}
+				}
+				else {
+					if (voisins[i][j] == 3) {
+						grille[i][j] = 1;
+					}
+				}
+			}
+		}
+
+		affiche();
+		return true;
+	}
+};
+
+int main() {
+	srand(time(NULL));
+
+	JeuDeLaVie jeu;
+	if (jeu.ConstructConsole(60, 60, 14, 14)) {
+		jeu.Start();
+	}
+			
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # JeuDeLaVie
+
+![Language](https://img.shields.io/badge/language-C%2B%2B-blue.svg)
+
+## Description
+
+Ce projet vise à créer une version du jeu de la vie de Conway en C++ en utilisant la librairie [consoleGameEngine](https://www.youtube.com/watch?v=cWc0hgYwZyc) de Javidx9.
+
+![Jeu de la vie](demo.gif)
